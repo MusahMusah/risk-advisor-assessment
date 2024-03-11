a066bb48d99a54d80f453fa694dc4de221c9bd15
MusahMusah <aliyumusahmusah@gmail.com>
2024-03-11 15:59:25 +0100

    implemented api logout

---
+++ app/Http/Controllers/Api/V1/Auth/LogoutUserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Responses\ApiSuccessResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogoutUserController extends Controller
+{
+    public function __invoke(Request $request): ApiSuccessResponse
+    {
+        $request->user()->tokens()->delete();
+
+        return new ApiSuccessResponse(
+            data: null,
+            message: 'Logged out successfully!',
+            status: Response::HTTP_CREATED
+        );
+    }
+}
